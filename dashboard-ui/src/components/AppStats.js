0a2ae9d76b144c55779f627b11d772953ca40df1
Ubuntu <azureuser@ACIT3855.wfpnlt3421te3bmwhvtrdoayrc.phxx.internal.cloudapp.net>
2024-04-08 01:21:01 +0000

    Update app and components with new changes

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -8,7 +8,7 @@ export default function AppStats() {
 
 	const getStats = () => {
 	
-        fetch(`http://<Cloud DNS>:8100/stats`)
+        fetch(`http://acit4850-group10.westus3.cloudapp.azure.com:8100/stats`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Stats")
